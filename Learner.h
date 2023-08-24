77f63b2eadd43f1434801cad9601398a33aadf7e
Harry <stanhl1@student.op.ac.nz>
2023-08-25 05:21:58 +1200

    Created Learner class

---
+++ Learner.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Person.h"
+
+class CourseAssessmentMarks;
+
+class Learner : public Person
+{
+    CourseAssessmentMarks* _courseAssessmentMarks;
+
+public:
+    Learner(int id, std::string firstName, std::string lastname, CourseAssessmentMarks* courseAssessmentMarks): Person(id, firstName, lastname) 
+    {
+        _courseAssessmentMarks = courseAssessmentMarks;
+    }
+};
